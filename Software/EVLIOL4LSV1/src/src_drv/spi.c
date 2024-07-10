949402c46dbd9165555d803cd53ae8011bda52dd
TiNredmc <thipok17@gmail.com>
2024-07-10 16:52:27 +0700

    Updated SPI FSM
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- Software/EVLIOL4LSV1/src/src_drv/spi.c
+++ Software/EVLIOL4LSV1/src/src_drv/spi.c
@@ -17,30 +17,97 @@ void spi_init(){
 }
 
 void spi_read(uint8_t *data, size_t len){
+	uint8_t spi_fsm = 0;	
+		
+	GPIOA->ODR &= ~(1 << SPI1_SS);	
 	
-	GPIOA->ODR &= ~(1 << SPI1_SS);
-	
-	while(len--){
-		while(!(SPI1->SR & SPI_SR_TXE));
-		SPI1->DR = 0x00;
-		while(!(SPI1->SR & SPI_SR_RXNE));
-		*data++ = SPI1->DR;
-	}
+	do{
+		
+		switch(spi_fsm){
+			case 0:// Send dummy data over SPI
+			{
+				*(__IO uint8_t *)&SPI1->DR = 0x00;
+				
+				spi_fsm = 1;
+			}	
+			break;
 
+			case 1:// Wait for RX data
+			{
+				if((SPI1->SR & (3 << 9)) != 0){
+					spi_fsm = 2;
+				}
+			}
+			break;
+			
+			case 2:// Read back
+			{
+				if(data != NULL)
+					*data++ = *(__IO uint8_t *)&SPI1->DR;
+				else
+					(void)*(__IO uint8_t *)&SPI1->DR;
+				
+				len--;
+				spi_fsm = 0;
+			}
+			break;
+			
+			default:
+				spi_fsm = 0;
+				return;
+		}
+		
+	}while(len);
+		
+	
 	GPIOA->ODR |= (1 << SPI1_SS);
 }
 
 void spi_Write(uint8_t *data, size_t len){
-	GPIOA->ODR &= ~(1 << SPI1_SS);
+	uint8_t spi_fsm = 0;	
+		
+	GPIOA->ODR &= ~(1 << SPI1_SS);	
 	
-	while(len--){
-		SPI1->DR = *data++;
-		while(!(SPI1->SR & SPI_SR_TXE));
-		while(!(SPI1->SR & SPI_SR_RXNE));
-		(void)SPI1->DR;
-	}
+	do{
+		
+		switch(spi_fsm){
+			case 0:// Send data to over SPI
+			{
+				if(data != NULL)
+					*(__IO uint8_t *)&SPI1->DR = *data++;
+				else	
+					*(__IO uint8_t *)&SPI1->DR = 0x00;
+				
+				spi_fsm = 1;
+			}	
+			break;
+
+			case 1:// Wait for RX data
+			{
+				if((SPI1->SR & (3 << 9)) != 0){
+					spi_fsm = 2;
+				}
+			}
+			break;
+			
+			case 2:// Read back dummy data
+			{
+				(void)*(__IO uint8_t *)&SPI1->DR;
+				
+				len--;
+				spi_fsm = 0;
+			}
+			break;
+			
+			default:
+				spi_fsm = 0;
+				return;
+		}
+		
+	}while(len);
+		
 	
-	GPIOA->ODR |= (1 << SPI1_SS);	
+	GPIOA->ODR |= (1 << SPI1_SS);
 }
 
 
@@ -56,7 +123,7 @@ void spi_WriteAndRead(
 	do{
 		
 		switch(spi_fsm){
-			case 0:// Send data to buffer
+			case 0:// Send data over SPI
 			{
 				if(TXbuf != NULL)
 					*(__IO uint8_t *)&SPI1->DR = *TXbuf++;
@@ -75,7 +142,7 @@ void spi_WriteAndRead(
 			}
 			break;
 			
-			case 2:
+			case 2:// Read back
 			{
 				if(RXbuf != NULL)
 					*RXbuf++ = *(__IO uint8_t *)&SPI1->DR;
@@ -103,27 +170,28 @@ void spi_WriteOnceAndRead(
 	uint8_t *RXbuf,
 	size_t len){
 
-	uint8_t spi_fsm;		
-	uint8_t tx_lock;
+	volatile uint8_t spi_fsm;		
 		
-	GPIOA->ODR &= ~(1 << SPI1_SS);	
+  GPIOA->ODR &= ~(1 << SPI1_SS);	
 	len++;
 
 	do{
 		
 		switch(spi_fsm){
-			case 0:// Send data to buffer
+			case 0:// Send data to SPI
 			{
-				if(tx_lock == 0){
-					*(__IO uint8_t *)&SPI1->DR = TX;
-					tx_lock = 1;
-				}else
-					*(__IO uint8_t *)&SPI1->DR = 0x00;
-				
+				*(__IO uint8_t *)&SPI1->DR = TX;
 				spi_fsm = 1;
 			}	
 			break;
 
+			case 3:// Send dummy data
+			{
+				*(__IO uint8_t *)&SPI1->DR = 0x00;
+				spi_fsm = 1;
+			}
+			break;
+			
 			case 1:// Wait for RX data
 			{
 				if((SPI1->SR & (3 << 9)) != 0){
@@ -132,7 +200,7 @@ void spi_WriteOnceAndRead(
 			}
 			break;
 			
-			case 2:
+			case 2:// Read back
 			{
 				if(RXbuf != NULL)
 					*RXbuf++ = *(__IO uint8_t *)&SPI1->DR;
@@ -140,7 +208,7 @@ void spi_WriteOnceAndRead(
 					(void)*(__IO uint8_t *)&SPI1->DR;
 				
 				len--;
-				spi_fsm = 0;
+				spi_fsm = 3;
 			}
 			break;
 			
@@ -160,27 +228,26 @@ void spi_WriteOnceAndWrite(
 	uint8_t *TXbuf,
 	size_t len){
 	
-	uint8_t tx_switch;	
-	uint8_t spi_fsm;		
+	volatile uint8_t spi_fsm;		
 
-		
 	GPIOA->ODR &= ~(1 << SPI1_SS);	
 	len++;
 
 	do{
 		
 		switch(spi_fsm){
-			case 0:// Send data to buffer
+			case 0:// Send address data over SPI
 			{
-				if(tx_switch == 0){
-					*(__IO uint8_t *)&SPI1->DR = TX;
-					tx_switch = 1;
-				}else{
-					*(__IO uint8_t *)&SPI1->DR = *TXbuf++;
-				}
-				
+				*(__IO uint8_t *)&SPI1->DR = TX;
 				spi_fsm = 1;
 			}	
+			break;
+			
+			case 3:// Send the Actual data over SPI
+			{
+				*(__IO uint8_t *)&SPI1->DR = *TXbuf++;
+				spi_fsm = 1;
+			}
 			break;
 
 			case 1:// Wait for RX data
@@ -191,12 +258,12 @@ void spi_WriteOnceAndWrite(
 			}
 			break;
 			
-			case 2:
+			case 2:// Read back
 			{
 				(void)*(__IO uint8_t *)&SPI1->DR;
 				
 				len--;
-				spi_fsm = 0;
+				spi_fsm = 3;
 			}
 			break;
 			
