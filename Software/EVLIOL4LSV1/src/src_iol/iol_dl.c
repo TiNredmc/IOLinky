f5003ed0bde25b8f90ba6a9df05f8888a748dab1
TiNredmc <thipok17@gmail.com>
2024-07-18 10:13:06 +0700

    Can reply to Direct parameter 1 and enter PREOPERATE and OPERATE mode. TODO : Reply ISDU in OPREATE MODE.
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- Software/EVLIOL4LSV1/src/src_iol/iol_dl.c
+++ Software/EVLIOL4LSV1/src/src_iol/iol_dl.c
@@ -6,4 +6,402 @@ iol_mtype_0		iol_mt0;
 iol_mtype_1_2 iol_mt1_2;
 #define MT1_2_LEN	4
 
+iol_isdu_Iserv iol_iservice;
 
+uint8_t dl_main_fsm = 0;
+uint8_t dl_startup_fsm = 0;
+uint8_t dl_mode_fsm = 0;
+
+uint8_t master_cmd = 0;
+uint8_t master_cycle = 0;
+
+void iol_dl_modeHandler();
+
+uint8_t isdu_attempt_cnt = 0;
+
+void iol_dl_handleSTARTUP();
+void iol_dl_T0WritePage(uint8_t address);
+void iol_dl_T0ReadPage(uint8_t address);
+void iol_dl_T0WriteISDU(uint8_t address);
+void iol_dl_T0ReadISDU(uint8_t address);
+
+void iol_dl_handlePREOPERATE();
+
+void iol_dl_handleOPERATE();
+void iol_dl_T12ReadISDU(uint8_t address);
+void iol_dl_T12WriteISDU(uint8_t address);
+
+void iol_dl_init(){
+	iol_pl_init(
+		(uint8_t *)&iol_mt0, 
+		(uint8_t *)&iol_mt0.OD
+	);
+}
+
+void iol_dl_poll(){
+	switch(dl_main_fsm){
+		case DL_MFSM_PR:// Wait for test message after wake up pulse
+		{
+			if(!iol_pl_ReadAvailable())
+				break;
+
+			// If read available. Read the data.
+			iol_dl_modeHandler();
+			
+			dl_main_fsm = DL_MFSM_WFWR;
+		}
+		break;
+		
+		case DL_MFSM_WFWR:
+		{ // Wait until reply (write) is done.
+			if(iol_pl_checkWriteStatus()){
+				GPIOB->ODR &= ~(1 << IOL_mon);
+				
+				// Make sure we are ready to reply on next cycle.
+				switch(dl_mode_fsm){
+					case DL_MODE_STARTUP:
+					case DL_MODE_PREOP:
+					{
+						iol_pl_update_WriteBuffer(&iol_mt0.OD);
+					}
+					break;
+					
+					case DL_MODE_OP:
+					{
+						iol_pl_update_WriteBuffer(iol_mt1_2.OD);
+					}
+					break;
+				}
+				
+				dl_main_fsm = DL_MFSM_PR;
+			}
+		}
+		break;
+	}
+		
+}
+
+void iol_dl_modeHandler(){
+
+	switch(dl_mode_fsm){
+		case DL_MODE_STARTUP:
+		{
+			iol_dl_handleSTARTUP();
+		}
+		break;
+		
+		case DL_MODE_PREOP:
+		{
+			iol_dl_handlePREOPERATE();
+		}
+		break;
+		
+		case DL_MODE_OP:
+		{
+			iol_dl_handleOPERATE();
+		}
+		break;
+	}
+	
+	switch(master_cmd){
+		case 0x5A:// Fallback to SIO but nah ¯\_(o_0)_/¯
+		case 0x97:// Switch to start up mode
+		{
+			iol_pl_setMtype0();
+			iol_pl_updateBuffer(
+				&iol_mt0.MC,
+				&iol_mt0.OD
+			);
+			dl_mode_fsm = DL_MODE_STARTUP;
+		}
+		break;
+		
+		case 0x99:// Switch to Operate mode
+		{
+			iol_pl_setMtype1_2();
+			iol_pl_updateBuffer(
+				&iol_mt1_2.MC,
+				iol_mt1_2.OD
+			);
+			dl_mode_fsm = DL_MODE_OP;
+		}	
+		break;
+		
+		case 0x9A:// Switch to Preoperate mode
+		{
+			iol_pl_setMtype0();
+			iol_pl_updateBuffer(
+				&iol_mt0.MC,
+				&iol_mt0.OD
+			);
+			dl_mode_fsm = DL_MODE_PREOP;
+		}
+		break;
+		
+		default:
+			break;
+	}
+	master_cmd = 0;// Clear master cmd for receive next cmd.
+	
+}
+
+void iol_dl_handleSTARTUP(){
+	
+	if(iol_mt0.MCBit.RW){
+		// Read mode
+		switch(iol_mt0.MCBit.CC){
+			case 0:// Process data
+				break;
+			case 1:// Page data
+				iol_dl_T0ReadPage(iol_mt0.MCBit.ADDR);
+				break;
+			case 3:// ISDU data
+				iol_dl_T0ReadISDU(iol_mt0.MCBit.ADDR);
+				break;
+			default:
+				break;
+		}
+	}else{
+		// Write mode
+		switch(iol_mt0.MCBit.CC){
+			case 0:// Process data
+				break;
+			case 1:// Page data
+				iol_dl_T0WritePage(iol_mt0.MCBit.ADDR);
+				break;
+			case 3:// ISDU data
+				iol_dl_T0WriteISDU(iol_mt0.MCBit.ADDR);
+				break;
+			default:
+				break;
+		}
+	}
+	
+}
+
+void iol_dl_T0WritePage(uint8_t address){
+		switch(address){
+			case 0x00:
+				master_cmd = iol_mt0.OD;
+				break;
+			case 0x01:
+				master_cycle = iol_mt0.OD;
+				break;
+			default:
+				return;
+		}	
+		iol_pl_update_WriteBuffer(&iol_mt0.CKS);
+		iol_mt0.CKS = 0x00;
+		iol_pl_WriteRequest(1);// Return Type 0 test message reply
+}
+
+void iol_dl_T0ReadPage(uint8_t address){
+		switch(address){
+		case 0x00:
+			iol_mt0.OD = 0x00;
+		break;
+		
+		case 0x01:
+			iol_mt0.OD = 0x00;
+		break;
+		
+		case 0x02:// Min cycle time
+			iol_mt0.OD = (0 << 6) | (40);// 0.1ms * 40 = 4ms min cycle time.
+		break;
+		
+		case 0x03:// M-sequence Capability
+			iol_mt0.OD = 
+				(0 << 4) |	// M-seq code type 0 for Preoperate	
+				(1 << 1);		// M-seq code type 1_2 for Operate
+		break;
+		
+		case 0x04:// IO-Link revision ID
+			iol_mt0.OD = 0x11;
+		break;
+		
+		case 0x05:// PD in properties
+		case 0x06:// PD out	proterties
+			iol_mt0.OD = 0x00;
+		break;
+		
+		case 0x07:// Vendor ID MSB
+		case 0x08:// Vendor ID LSB
+			iol_mt0.OD = 0x69;
+		break;
+		
+		case 0x09:// DevID MSB
+			iol_mt0.OD = 0x69;
+		break;
+		
+		case 0x0A:// DevID
+			iol_mt0.OD = 0x04;
+		break;
+		
+		case 0x0B:// DevID LSb
+			iol_mt0.OD = 0x20;
+		break;
+		
+		case 0x0C:// Function ID MSB
+		case 0x0D:// Function ID LSB
+			iol_mt0.OD = 0x00;
+			break;
+		
+		case 0x0E:// Reserved
+			iol_mt0.OD = 0x00;
+			break;
+		
+		case 0x0F:// System command
+			iol_mt0.OD = 0x00;
+			break;
+		
+		default:
+			iol_mt0.OD = 0x00;
+			break;
+	}
+	
+	iol_mt0.CKS = 0x00;
+	
+	iol_pl_WriteRequest(2);// Return Type 0 test message reply
+}
+
+uint8_t ISDU_data_pointer = 0;
+uint8_t ISDU_buffer[12] = {0};
+void iol_dl_T0WriteISDU(uint8_t address){
+	
+	if(address < 0x10){
+		ISDU_buffer[address] = iol_mt0.OD;
+		goto reply_cks;
+	}
+	
+	switch(address){
+		case 0x10:// Get the ISDU I-service
+				iol_iservice.Iservice = iol_mt0.OD;
+			break;
+		
+		default:
+			break;
+	}
+
+reply_cks:
+	iol_pl_update_WriteBuffer(&iol_mt0.CKS);
+	iol_mt0.CKS = 0x00;
+	iol_pl_WriteRequest(1);// Return Type 0 test message reply
+}
+
+uint8_t prev_FlowCTRL = 0;
+uint8_t ISDU_FC_count = 0;
+void iol_dl_T0ReadISDU(uint8_t address){
+	if(prev_FlowCTRL == address){
+		// No FlowCTRL Address change
+		// NACK to let master resend
+	}	
+	
+	isdu_attempt_cnt++;
+
+	// Grab the M-seq count
+	if(address < 0x10){
+		ISDU_FC_count = address;
+	}
+	
+	switch(address){
+		case 0x10:// FlowCTRL START, send read respond I-service
+				iol_mt0.OD = 0xD0 | (1);// I-service is Read response(+) and the data length is
+			break;
+		
+		case 0x11:// FlowCTRL IDLE1
+			// Reply with OD = 0x00
+			GPIOB->ODR |= (1 << IOL_mon);
+			iol_mt0.OD = 0x00;
+			iol_mt0.CKS = 0x00;
+			iol_pl_WriteRequest(2);// Return Type 0 test message reply
+			goto exit_t0isdu_ja;
+		
+		case 0x1F:// FlowCTRL ABORT
+			break;
+		
+		case 0x12:// FlowCTRL IDLE2 (reserved for future use, no use here for now...)
+		default:// 0x13 to 0x1E is reserved
+			break;
+	}
+	
+	iol_pl_update_WriteBuffer(&iol_mt0.CKS);
+	iol_mt0.CKS = 0x00;
+	iol_pl_WriteRequest(1);// Return Type 0 test message reply
+
+exit_t0isdu_ja:
+	prev_FlowCTRL = address;
+}
+
+void iol_dl_handlePREOPERATE(){
+	
+	if(iol_mt0.MCBit.RW){
+		switch(iol_mt0.MCBit.CC){
+			case 0:// Process data
+				break;
+			case 1:// Page data
+				iol_dl_T0ReadPage(iol_mt0.MCBit.ADDR);
+				break;
+			case 3:// ISDU data
+				iol_dl_T0ReadISDU(iol_mt0.MCBit.ADDR);
+				break;
+			default:
+				break;
+		}
+	}else{
+		switch(iol_mt0.MCBit.CC){
+			case 0:// Process data
+				break;
+			case 1:// Page data
+				iol_dl_T0WritePage(iol_mt0.MCBit.ADDR);
+				break;
+			case 3:// ISDU data
+				//iol_dl_T0WriteISDU(iol_mt0.MCBit.ADDR);
+				break;
+			default:
+				break;
+		}
+	}
+	
+}
+
+void iol_dl_handleOPERATE(){
+
+	if(iol_mt1_2.MCBit.RW){
+		// Read mode
+		switch(iol_mt1_2.MCBit.CC){
+			case 0:// Process data
+				break;
+			case 1:// Page data
+				//iol_dl_T12ReadPage(iol_mt1_2.MCBit.ADDR);
+				break;
+			case 3:// ISDU data
+				iol_dl_T12ReadISDU(iol_mt1_2.MCBit.ADDR);
+				break;
+			default:
+				break;
+		}
+	}else{
+		// Write mode
+		switch(iol_mt1_2.MCBit.CC){
+			case 0:// Process data
+				break;
+			case 1:// Page data
+				//iol_dl_T0WritePage(iol_mt1_2.MCBit.ADDR);
+				break;
+			case 3:// ISDU data
+				iol_dl_T0WriteISDU(iol_mt1_2.MCBit.ADDR);
+				break;
+			default:
+				break;
+		}
+	}
+	
+}
+
+void iol_dl_T12ReadISDU(uint8_t address){
+
+}
+
+void iol_dl_T12WriteISDU(uint8_t address){
+
+}
--- Software/EVLIOL4LSV1/src/src_iol/iol_dl.h
+++ Software/EVLIOL4LSV1/src/src_iol/iol_dl.h
@@ -2,22 +2,37 @@
 #define IOL_DL_H
 
 #include <stdint.h>
+#include <stm32g071xx.h>
+#include "iol_pl.h"
+#include "gpio_pinout.h"
 
-typedef struct{
+// DL Main FSM enums
+enum DL_MAIN_FSM{
+	DL_MFSM_PR	= 0,// Wait for message read
+	DL_MFSM_WFWR		// Wait for reply done
+};
+
+enum DL_MODE_FSM{
+	DL_MODE_STARTUP = 0,
+	DL_MODE_PREOP,
+	DL_MODE_OP
+};
+
+typedef struct __attribute__((packed)){
 	union{
 		uint8_t MC;
 		struct{
-			uint8_t RW		:1;// Read/Write
-			uint8_t CC		:2;// Commu Channel
 			uint8_t ADDR	:5;// Address
+			uint8_t CC		:2;// Commu Channel
+			uint8_t RW		:1;// Read/Write
 		}MCBit;
 	};
 	
 	union{
 		uint8_t CKT;
 		struct{
-			uint8_t MT		:2;// M-sequence type
 			uint8_t CKS		:6;// Checksum
+			uint8_t MT		:2;// M-sequence type
 		}CKTBit;
 	};
 	
@@ -26,59 +41,30 @@ typedef struct{
 	union{
 		uint8_t CKS;
 		struct{
-			uint8_t EV		:1;// Event flag
-			uint8_t PD		:1;// PD status
 			uint8_t CKS		:6;// Checksum
+			uint8_t PD		:1;// PD status
+			uint8_t EV		:1;// Event flag
 		}CKSBit;
 	};
 }iol_mtype_0;
 
-typedef struct{
-	union{
-		uint8_t MC;
-		struct{
-			uint8_t RW		:1;// Read/Write
-			uint8_t CC		:2;// Commu Channel
-			uint8_t ADDR	:5;// Address
-		}MCBit;
-	};
-	
-	union{
-		uint8_t CKT;
-		struct{
-			uint8_t MT		:2;// M-sequence type
-			uint8_t CKS		:6;// Checksum
-		}CKTBit;
-	};
-	
-	uint8_t PD[2];// Process Data
-	
-	union{
-		uint8_t CKS;
-		struct{
-			uint8_t EV		:1;// Event flag
-			uint8_t PD		:1;// PD status
-			uint8_t CKS		:6;// Checksum
-		}CKSBit;
-	};
-	
-}iol_mtype_1_1;
 
-typedef struct{
+
+typedef struct __attribute__((packed)){
 	union{
 		uint8_t MC;
 		struct{
-			uint8_t RW		:1;// Read/Write
-			uint8_t CC		:2;// Commu Channel
 			uint8_t ADDR	:5;// Address
+			uint8_t CC		:2;// Commu Channel
+			uint8_t RW		:1;// Read/Write
 		}MCBit;
 	};
 	
 	union{
 		uint8_t CKT;
 		struct{
-			uint8_t MT		:2;// M-sequence type
 			uint8_t CKS		:6;// Checksum
+			uint8_t MT		:2;// M-sequence type
 		}CKTBit;
 	};
 	
@@ -87,23 +73,30 @@ typedef struct{
 	union{
 		uint8_t CKS;
 		struct{
-			uint8_t EV		:1;// Event flag
-			uint8_t PD		:1;// PD status
 			uint8_t CKS		:6;// Checksum
+			uint8_t PD		:1;// PD status
+			uint8_t EV		:1;// Event flag
 		}CKSBit;
 	};
 	
 }iol_mtype_1_2;
 
+typedef struct{
+	union{
+		uint8_t Iservice;
+		struct{
+			uint8_t length:4;
+			uint8_t Iserv	:4;
+		};		
+	};
+
+}iol_isdu_Iserv;
 
 
+void iol_dl_init();
 
-
-
-
-
-
-
+void iol_dl_poll();
+uint8_t iol_dl_getStatus();
 
 
 #endif
