e86fe7ba3b1db970637433f87715d5f36c733fe2
TiNredmc <thipok17@gmail.com>
2024-07-18 10:11:24 +0700

    Now using COM2 and changed the polling method.
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- Software/EVLIOL4LSV1/src/src_iol/iol_pl.c
+++ Software/EVLIOL4LSV1/src/src_iol/iol_pl.c
@@ -11,6 +11,8 @@ uint8_t current_mtype = 0;
 uint8_t pl_write_fsm = 0;
 uint8_t write_request = 0;
 
+uint8_t read_available = 0;
+
 uint8_t alive_led = 0;
 
 void iol_pl_init(
@@ -21,104 +23,95 @@ void iol_pl_init(
 	read_buffer_ptr = rd_buffer_ptr;	
 	write_buffer_ptr = wr_buffer_ptr;
 		
-	l6364_setIOLmode();	
-	l6364_setCOM(COM3);// COM3 mode	
-	l6364_setLED1(0x01);// Arbitrary number to set LED on. Indicates the successful SPI comm. 
-
+	l6364_setIOLmode();// Put chip into IO-Link mode
+	l6364_setCOM(COM2);// COM2 mode	
+	l6364_setLED1(0x00);	
+	l6364_setLED2(0x00);
+		
 	// Startup with Type-0 to detect wakeup sequence
 	iol_pl_setMtype0();
 }
 	
+void iol_pl_update_ReadBuffer(uint8_t *rd){
+	read_buffer_ptr = rd;
+}
+
+void iol_pl_update_WriteBuffer(uint8_t *wr){
+	write_buffer_ptr = wr;
+}
+	
+void iol_pl_updateBuffer(
+	uint8_t *rd_buffer_ptr,
+	uint8_t *wr_buffer_ptr
+	){
+	read_buffer_ptr = rd_buffer_ptr;	
+	write_buffer_ptr = wr_buffer_ptr;
+}
+
 void iol_pl_aliveLED(){
-	if(alive_led > 0x04){
+	if(alive_led > 0x08){
 		alive_led--;
 		if(alive_led == 0x10)
 			alive_led = 0;
 	}else{
 		alive_led++;
-		if(alive_led == 0x04)
-			alive_led = 0x14;
+		if(alive_led == 0x08)
+			alive_led = 0x18;
 	}
 	
-	l6364_setLED2(alive_led & 0x0F);
+	l6364_setLED1(alive_led & 0x0F);
 }
 
-void iol_pl_poll(){
-	l6364_pollAllReg();
+void iol_pl_pollRead(){
+		read_available = l6364_readFIFOFast(read_buffer_ptr);
 	
-	// if data present in the buffer
-	if(l6364_isDAT()){
-		pl_rx_iol_byte = l6364_pollLinkCnt();
-		l6364_readFIFO(
-			pl_rx_iol_byte,
-			read_buffer_ptr
+	if(l6364_isCHK()){
+		l6364_linkEnd();
+		read_available = 0;
+	}
+}
+
+void iol_pl_pollWrite(){
+	// Data write
+	if(write_request){
+		write_request = 0;
+		l6364_writeFIFOFast(
+			pl_tx_iol_byte,
+			write_buffer_ptr
 		);
 	}
-	
-	// Data Write FSM 
-	switch(pl_write_fsm){
-		case 0:// Wait for write request flag
-		{
-			if(write_request){
-				// make sure that write buffer (source) is not NULL
-				if(write_buffer_ptr == NULL)
-					break;
-				
-				if(pl_tx_iol_byte == 0)
-					break;
-				
-				// clear flag
-				write_request = 0;
-				
-				// Set Link (data) count
-				l6364_setTXCount(pl_tx_iol_byte);
-				
-				pl_write_fsm = 1;
-			}
-		}
-		break;
-		
-		case 1:// Write data to L6364
-		{
-			l6364_writeFIFO(
-				pl_tx_iol_byte,
-				write_buffer_ptr
-			);
-			
-			pl_write_fsm = 2;
-		}
-		break;
-		
-		case 2:// Check status
-		{
-		
-			
-			pl_write_fsm = 0;
-		}
-		break;
-		
-		default:
-			pl_write_fsm = 0;
-			break;
-	
-	}
-	
-
 }
 
-void iol_pl_dlReadBuffer(){
- 
- 
+uint8_t iol_pl_ReadAvailable(){
+	if(read_available == 0)
+		return 0;
+	
+	read_available = 0;
+	return 1;
 }
 
-void iol_pl_dlWriteRequest(uint8_t count){
+void iol_pl_WriteRequest(uint8_t count){
 	pl_tx_iol_byte = count;
 	write_request = 1;
 }
 
+uint8_t iol_pl_checkWriteStatus(){
+	return !write_request;
+}	
+
+void iol_pl_LinkSND(){
+	l6364_linkSend();
+}
+
+void iol_pl_LinkEND(){
+	l6364_linkEnd();
+}
+
+// ========= Setting M-Sequence Type ===============
 uint8_t iol_pl_getCurrentMtype(){
 	return current_mtype;
 }
+
 
 void iol_pl_setMtype0(){
 	l6364_setMseq(
@@ -137,3 +130,5 @@ void iol_pl_setMtype1_2(){
 	);
 	current_mtype = MTYPE_1_2;
 }
+
+// ========= END Set M-Seq Type ====================
--- Software/EVLIOL4LSV1/src/src_iol/iol_pl.h
+++ Software/EVLIOL4LSV1/src/src_iol/iol_pl.h
@@ -14,10 +14,25 @@ void iol_pl_init(
 	uint8_t *rd_buffer_ptr,
 	uint8_t *wr_buffer_ptr
 	);
+
+void iol_pl_update_ReadBuffer(uint8_t *rd);
+void iol_pl_update_WriteBuffer(uint8_t *wr);
+void iol_pl_updateBuffer(
+	uint8_t *rd_buffer_ptr,
+	uint8_t *wr_buffer_ptr
+	);
+
 void iol_pl_aliveLED();
-void iol_pl_poll();
-void iol_pl_dlReadBuffer();
-void iol_pl_dlWriteBuffer();
+void iol_pl_pollRead();
+void iol_pl_pollWrite();
+
+// Available for IOL Data Link layer
+uint8_t iol_pl_ReadAvailable();
+void iol_pl_WriteRequest(uint8_t count);
+uint8_t iol_pl_checkWriteStatus();
+void iol_pl_LinkSND();
+void iol_pl_LinkEND();
+
 uint8_t iol_pl_getCurrentMtype();
 void iol_pl_setMtype0();
 void iol_pl_setMtype1_2();
