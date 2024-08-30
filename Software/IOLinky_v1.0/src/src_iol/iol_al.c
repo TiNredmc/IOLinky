9baf5db90b1bc610abf73f6e1f689db5dd515371
TiNredmc <thipok17@gmail.com>
2024-08-30 10:03:15 +0700

    PD cycle is now 20ms and using TYP2_V message with 8 PD in data.
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- Software/IOLinky_v1.0/src/src_iol/iol_al.c
+++ Software/IOLinky_v1.0/src/src_iol/iol_al.c
@@ -4,25 +4,31 @@
 device_directparam_t device_dp_t = {
 	// Page 1
 	{
-		0,					// MasterCommand
-		0,					// MasterCycleTime
-		(0 << 6) | 
-		(40),					// MinCycleTime
-		PREOP_M_0 | 
-		OP_M_2_2 | 1,					// MSeqenceCapability
-		0x11,					// RevisionID
-		16,					// ProcessDataIn 16 Bit
-		0,					// ProcessDataOut;
-		0xFFFF,				// VendorID
-		0x563412,				// DeviceID
-		0,					// FunctionID
-		0,					// --Reserved--
-		0						// SystemCommand
+		0,									// MasterCommand
+		0,									// MasterCycleTime
+		
+		(1 << 6) | 
+		(34),								// MinCycleTime -> 20ms
+		
+		PREOP_M_0 | 				// MSeqenceCapability
+		OP_M_2_V8 | 1,			// OPERATE : TYPE2_V 8 PDin octets
+		
+		0x11,								// RevisionID IO-Link V1.1
+		(1 << 7) | (8 - 1),	// ProcessDataIn -> 8 octets
+		0,									// ProcessDataOut -> 0
+		0xFFFF,							// VendorID
+		0x563412,						// DeviceID
+		0,									// FunctionID
+		0,									// --Reserved--
+		0										// SystemCommand
 	},
 	
 	// Page 2 // Device Specific profile
 	{
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16					
+		1, 2, 3, 4,
+		5, 6, 7, 8, 
+		9, 10, 11, 12, 
+		13, 14, 15, 16					
 	}
 	
 };
@@ -38,14 +44,15 @@ isdu_data_t isdu_d_t = {
 	"F042"
 };
 
-uint16_t PD_cpy  = 0;
 // Initialize the underlying layer (AL->DL->PL)
-void iol_al_init(){
+void iol_al_init(
+	uint8_t *PD_data_ptr	
+	){
 	iol_dl_init(
 		(uint8_t *)&device_dp_t,	
 		&isdu_d_t,
-		(uint8_t *)&PD_cpy,
-		1
+		(uint8_t *)PD_data_ptr,
+		8			// 8 octets for TYPE2_V with 8 PD in
 	);
 }
 
@@ -56,7 +63,6 @@ void iol_al_poll(){
 }
 
 // Demo used to update Process Data.
-void iol_al_updatePD(uint16_t pd_val){
-	PD_cpy = pd_val;
+void iol_al_updatePD(){
 	iol_dl_updatePD();
 }
